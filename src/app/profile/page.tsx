08db5f647995ec2564ed68f0dff11d15daca6be6
M Junaid <134502135+junaid042@users.noreply.github.com>
2026-03-05 00:16:48 +0500

    changed wiki name

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -107,7 +107,7 @@ const Profile = () => {
 
             <div className="mt-4">
               <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
-                Waqas Ahmed
+                Junaid Azhar
               </h3>
               <p className="font-medium">Software Engineer</p>
               <div className="mx-auto mb-5.5 mt-4.5 grid max-w-94 grid-cols-3 rounded-md border border-stroke py-2.5 shadow-1 dark:border-strokedark dark:bg-[#37404F]">
